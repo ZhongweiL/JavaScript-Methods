5de3cefa201639c46185bcf4b3df41c4b6f61be4
Zhongwei Li <li@Zhongweis-Air.lan>
2022-03-07 22:43:17 -0500

    load functions implemented in class

--- methods.js
+++ methods.js
@@ -1,6 +1,13 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
-  // Place your code here.
+Array.prototype.myEach = function (callbackFn) {
+    for (let i = 0; i < this.length; i++) {   // "this" keyword refers to the array being called.
+        if (this[i] === undefined) continue;
+        // callbackFn can take up to 3 input parameters:
+        // element
+        // element, index
+        // element, index, array
+        callbackFn(this[i], i, this);   // callbackFn is "console.log(x,i,myArray)" 
+    }
 };
 
 // MAP //
@@ -29,8 +36,15 @@ Array.prototype.myIndexOf = function() {
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
-  // Place your code here.
+Array.prototype.myPush = function(...args) {  // Use rest parameter to accept any number of input arguments
+  let args_index = 0;  // Index of new element
+  let length = this.length;   // Length of "this" array
+  // The last element of "this" array is at length - 1
+  for (let i = length; i < length + args.length; i++) {
+      this[i] = args[args_index];  // Add new element to end of "this" array
+      args_index++;
+  }
+  return this.length;  // Return new length of "this" array
 };
 
 // LASTINDEXOF //
